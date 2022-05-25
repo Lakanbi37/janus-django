9fb448047a2c9ff948396a295ab86ee90c638ead
Lekan Akanbi <lakanbi37@gmail.com>
2022-05-25 00:48:11 -0700

    added live stream model

--- src/apps/stream/models.py
+++ src/apps/stream/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from apps.security.models import Encryption
 
 # Create your models here.
+
+
+class LiveStream(Encryption):
+    channel = models.ForeignKey("channel.Channel", on_delete=models.CASCADE, related_name="live_streams")
+    name = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    max_viewers = models.IntegerField(blank=True, null=True)
+    code = models.SlugField(null=True, blank=True)
+    pin = models.CharField(null=True, blank=True)
+    record = models.BooleanField(default=False)
+    record_path = models.CharField(max_length=220, null=True, blank=True)
+    feeds = models.ManyToManyField("events.Feed", null=True, blank=True, related_name="live_streams")
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
